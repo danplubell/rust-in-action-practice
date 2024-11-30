196d3f0792d02f2063ec84a4e5b30623a2ee5566
Dan Plubell <danplubell@gmail.com>
2024-11-29 19:47:21 -0600

    Update initial to use stdin

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,25 +1,52 @@
-use regex::Regex;
 use clap::{App, Arg};
+use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 
+fn process_lines<T:BufRead + Sized>(reader: T, re:Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_)=> println!("{}", line),
+            None => ()
+        }
+    }
+}
 fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
         .about("searches for patterns")
-        .arg(Arg::with_name("pattern")
-        .help("The pattern to search for")
-        .takes_value(true)
-        .required(true))
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
+    /*
     let quote = "\
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
-What do we seek through millions of pages?";     // <1>
+What do we seek through millions of pages?"; // <1>
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);// <2>
-        if let Some(_) = contains_substring { println!("{}", line) }
+
+     */
+    let input = args.value_of("input").unwrap_or("-");
+    if input == "-" {
+        let stdin = io::stdin();
+        
     }
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 }
