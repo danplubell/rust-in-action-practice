149cdb421d22cf80086b9594bcb58bdd5dcbed6b
Dan Plubell <danplubell@gmail.com>
2024-12-02 18:54:01 -0600

    Add mailbox

--- ch4/ch4-check-sats-3/src/main.rs
+++ ch4/ch4-check-sats-3/src/main.rs
@@ -3,8 +3,18 @@
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
+    mailbox: Mailbox,
 }
-
+impl CubeSat {
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
+}
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>
+}
+type Message = String;
 #[derive(Debug)]
 enum StatusMessage {
     Ok,
@@ -14,11 +24,16 @@ fn check_status(sat_id: CubeSat) -> CubeSat {
     println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
     sat_id
 }
-
+struct GroundStation;
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
 fn main () {
-    let sat_a = CubeSat { id: 0 };
-    let sat_b = CubeSat { id: 1 };
-    let sat_c = CubeSat { id: 2 };
+    let sat_a = CubeSat { id: 0, mailbox: Mailbox{messages: vec![]} };
+    let sat_b = CubeSat { id: 1, mailbox: Mailbox{messages: vec![]}  };
+    let sat_c = CubeSat { id: 2, mailbox: Mailbox{messages: vec![]} };
 
     let sat_a = check_status(sat_a);   // <1>
     let sat_b = check_status(sat_b);
